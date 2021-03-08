87bace194d019637509b8cc65307b016a481dede
Tyler Linen <66691586+tylerlinen@users.noreply.github.com>
2021-03-08 16:14:09 -0500

    updated

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div className="container">
+            <h1>HomePage</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, quam voluptatibus. Doloribus dicta fugit deserunt natus voluptatem magni dolores harum iste, cumque provident. Quidem nam fugiat perspiciatis nesciunt repellendus ratione!</p>
+        </div>
+    )
+}
+
+export default Home
---
+++ frontend/src/components/Notes.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Notes() {
+    return (
+        <div className="container">
+            <h1>Notes Page</h1>
+        </div>
+    )
+}
+
+export default Notes
